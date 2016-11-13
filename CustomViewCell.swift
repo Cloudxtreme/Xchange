2283a0ccf081ad004131dfae4072595644f1d44f
drknature <jeffreyli@Jeffreys-MacBook-Pro-2.local>
2016-11-13 00:36:38 -0500

    adding files' '

---
+++ CustomViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomViewCell.swift
+//  Xchange
+//
+//  Created by Jeffrey Li on 11/12/16.
+//  Copyright © 2016 Jeffrey Li. All rights reserved.
+//
+
+import UIKit
+
+class CustomViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+
+pod 'Firebase/Core'
+pod 'Firebase/Database'
+pod 'Firebase/Auth'
+
+target 'Xchange' do
+pod 'GoogleMaps'
+pod 'GooglePlaces'
+end
